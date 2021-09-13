d78f6931705c6d88eb9abaebd96ff10890c41306
scouarn <scouarn.contact@gmail.com>
2021-09-13 22:28:44 +0200

    expressions

--- regex.ml
+++ regex.ml
@@ -1,107 +1,41 @@
-(* Apply f to every element *)
-let rec map f = function 
-	| [] -> []
-	| h::t -> (f h)::(map f t)
-;;
+ 
+(* The regex is a special case of parser where it only deals with
+ * lists of chars, where the empty list [] is the empty word empsilon or "".
+ *)
 
-(* Apply x1 + x2 + x3... (+ = f) *)
-let rec assos el_neutre f = function
-	| [] -> el_neutre
-	| h::[] -> h (* useful case ?*)
-	| h::t  -> f h (assos el_neutre f t)
-;;
+type regex = (char, char list) parser;;
 
+(* Versions with explicite regex type *)
+let parse_char : char->regex = canon;;
+let epsilon    : regex = empty;;
 
-(* string -> char list *)
-let string_to_list str = 
-
-	let rec aux l = function
-		| 0 -> l
-		| n -> aux ( (str.[n-1]) :: l) (n-1)
-
-	in aux [] (String.length str)
-;;
+let parse_any_chars l : regex =	parse_any_of (map parse_char l);;
+let parse_all_chars l : regex =	parse_all_of (map parse_char l);;
 
 
-
-(* Parser defs *)
-type 'a maybe = None | Just of 'a;;
-type ('a, 'b) parser = 'a list -> 'a list * 'b maybe list;; (* maybe [Just of char], maybe [None], maybe [] (failure case) *)
-type regex = (char, char) parser;;
-
-(* Parse empty *)
-let epsilon : regex = function
-	| input -> input, [None]
-;;
-
-(* The first if successful, OR the second *)
-let (<|>) (p1 : regex) (p2 : regex) : regex = fun input ->
-	match p1 input with
-	| rem, [] -> p2 input
-	| x -> x
-	
-;;
-
-(* The first AND the second *)
-let (<*>) (p1 : regex) (p2 : regex) : regex = fun input ->
-	match p1 input with
-	| rem, [] -> input, []
-	| rem1, res1 -> 
-		match p2 rem1 with
-		| rem2, [] -> input, []
-		| rem2, res2 -> rem2, res1 @ res2
-;;
+let parse_string str  : regex = clump (parse_all_chars (string_to_list str));;
 
 
-(* One or more *)
-let rec plus (p : regex) : regex = function input ->
-	((p <*> plus p) <|> p) input	(* BUG ? Does not work without applying input *)
-;;
+(* Elemtary sets *)
+let p0__1 = parse_any_chars ['0'; '1'];;
+let p0__7 = parse_any_chars (string_to_list "01234567");;
+let p0__9 = parse_any_chars (string_to_list "0123456789");;
+let p0__F = parse_any_chars (string_to_list "0123456789ABCDEF");;
+let pa__z = parse_any_chars (string_to_list "abcdefghijklmnopqrstuvwxyz");;
+let pA__Z = parse_any_chars (string_to_list "ABCDEFGHIJKLMNOPQRSTUVWXYZ");;
 
-(* Zero or more *) 
-let rec star (p : regex) : regex = plus p <|> epsilon;;
+let latin    : regex = pa__z <|> pA__Z;;
+let alphanum : regex = latin <|> p0__9;;
 
-
-(* Simple parsers *)
-let parse_char (c : char) : regex = function
-	| h::t when h = c -> t, [Just(h)]
-	| input -> input, [] 
-;;
-
-let parse_any_of = assos epsilon (<|>);;
-let parse_all_of = assos epsilon (<*>);; 
-
-let parse_any_chars l =	parse_any_of (map parse_char l);;
-let parse_all_chars l =	parse_all_of (map parse_char l);;
-
-let parse_string s = parse_all_chars (string_to_list s);;
-let parse_any_from_string s = parse_any_chars (string_to_list s);;
-
-let digit = parse_any_from_string "0123456789";;
-let parse_int   = plus digit;;
-let parse_float = parse_int <*> (parse_char '.') <*> parse_int;;
-
-let letter_lower = parse_any_from_string "abcdefghijklmnopqrstuvwxyz";;
-let letter_upper = parse_any_from_string "ABCDEFGHIJKLMNOPQRSTUVWXYZ";;
-let letter = letter_upper <|> letter_lower;;
-
-let identifier = plus (letter <|> digit);;
-
-let sspace = star (parse_char ' ');;
-
-let padded p = sspace <*> p <*> sspace;;
-
-let token_plus = padded (parse_char '+');;
-let token_mult = padded (parse_char '*');;
-let token_lpar = padded (parse_char '(');;
-let token_rpar = padded (parse_char ')');;
-
-(* To be defined via rec *)
-let sum = parse_int <*> token_plus <*> parse_int;;
-let prod = sum <*> token_mult <*> sum;;
-let expr = prod <*> token_plus <*> prod;;
+let space = parse_char ' ';;
 
 
+(* Utility *)
+
+let parse_int   : regex = clump (plus p0__9);;
+let parse_float : regex = parse_int <*> (parse_char '.') <*> parse_int;;
 
 
+let identifier : regex = latin <*> (star alphanum);;
 
+let padded (p : regex) : regex = (star space) <*> p <*> (star space);;
